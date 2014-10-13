a6048962b7c34fc85ec61f82fbfc0e2ee9e93743
Jennifer <jennifer@ravenwilde.com>
2014-10-13 17:49:22 -0400

    single-instagram_post.php working

--- content-instagram.php
+++ content-instagram.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Aqua-Instagram
+Plugin Name: Content Type Instagram
 Plugin URI: https://github.com/ravenwilde/content-instagram
-Description: A plugin that creates a custom post type for instagram imports - designed for use with my 'Aqua-One' Theme but can be used with any WordPress theme..
+Description: A plugin that creates a custom post type for instagram imports - comes with minimal styling, can be used with any WordPress theme.
 Version: 1.0
 Author: Jennifer Scroggins
 Author URI: http://www.ravenwilde.com/
@@ -12,7 +12,7 @@ License: GPLv2
 
 /* Instagram Custom Post Type */
 
-function register_post_type() {
+function register_instagram_post_type() {
 
     $labels = array(
         'name'                => _x( 'Instagrams', 'Post Type General Name', 'text_domain' ),
@@ -66,11 +66,40 @@ function register_instagram_styles()
     // Register the style like this for a plugin:  
     wp_register_style( 'instagram-style', plugins_url( '/css/content-instagram-style.css', __FILE__ ), array(), '20141013', 'all' );  
     wp_enqueue_style( 'instagram-style' );  
+}
+
+/* Add Instagram Posts to the Main Query */
+function content_instagram_query( $query ) {
+    
+    if( is_home() && $query->is_main_query() ) {
+        
+        $query->set( 'post_type', array('post', 'instagram_post') );
+        return $query;
+    }
+ 
 }  
+
+/* Add Custom Templates */
+function include_template_function( $template_path ) {
+    if ( get_post_type() == 'instagram_post' ) {
+        if ( is_single() ) {
+            // checks if the file exists in the theme first,
+            // otherwise serve the file from the plugin
+            if ( $theme_file = locate_template( array ( 'single-instagram_post.php' ) ) ) {
+                $template_path = $theme_file;
+            } else {
+                $template_path = plugin_dir_path( __FILE__ ) . '/single-instagram_post.php';
+            }
+        }
+    }
+    return $template_path;
+}
   
 /* Make everything happen */
-add_action( 'init', 'register_post_type', 0 );
+add_action( 'init', 'register_instagram_post_type', 0 );
 add_action( 'wp_enqueue_scripts', 'register_instagram_styles' );
+add_action( 'pre_get_posts', 'content_instagram_query' );
+add_filter( 'template_include', 'include_template_function', 1 );
 
 
 ?>
